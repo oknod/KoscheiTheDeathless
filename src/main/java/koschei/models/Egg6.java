7760a7a485f0106527cace417f83c9f1408ba2f3
oknod <114230918+oknod@users.noreply.github.com>
2023-03-06 05:55:57 +0300

    done

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,10 +1,12 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Egg6 {
     private final Needle7 needle;
+    @Autowired
     public Egg6 (Needle7 needle) {this.needle = needle;}
     @Override
     public String toString() {
--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,13 +1,17 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
-    private final Duck5 duck;
-    public Rabbit4(Duck5 duck){
+
+    private Duck5 duck;
+    /*public Rabbit4(Duck5 duck){
         this.duck = duck;
-    }
+    }*/
+    @Autowired
+    public void setDuck(Duck5 duck) {this.duck = duck;}
 
     @Override
     public String toString() {
